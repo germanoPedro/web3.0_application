146d219ec26274ee1bdd4b5d36bc1311fbd5374c
Pedro Germano <ptdc.germano@gmail.com>
2022-06-28 13:23:41 +0200

    tailwind config, loader, Welcome screen, 10301

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -5,9 +5,25 @@ import { Loader } from './'
 const commonStyles =
   'min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white'
 
+//our inputs will be put into a compoent since the inputs are similar in nature and dynamic
+const Input = ({ placeholder, name, type, value, handChange }) => (
+  //its gonna be normal jsx code so no need for cirly brackets
+  <input
+    placeholder={placeholder}
+    type={type}
+    step='0.0001' //ethereum values are very high precision so we need to set a step
+    value={value}
+    onChange={(e) => handChange(e, name)}
+    className='my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism'
+  />
+)
+
 const Welcome = () => {
   //connect the wallet to the website
   const connectWallet = () => {}
+
+  //for the loader
+  const handleSubmit = () => {}
 
   return (
     <div className='flex w-full justify-center items-center'>
@@ -32,6 +48,76 @@ const Welcome = () => {
             {/* create a div for each specific cell */}
 
             <div className={`rounded-tl-2xl ${commonStyles}`}>Reliability</div>
+            <div className={commonStyles}>Security</div>
+            <div className={`rounded-tr-2xl ${commonStyles}`}>Ethereum</div>
+            <div className={`rounded-bl-2xl ${commonStyles}`}>Web 3.0</div>
+            <div className={commonStyles}>Low Fees</div>
+            <div className={`rounded-br-2xl ${commonStyles}`}>Blockchain</div>
+          </div>
+        </div>
+        {/* end of left side */}
+
+        {/* right side of our desktop welcome view */}
+        <div className='flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10'>
+          {/* for the card and form */}
+          <div className='p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism'>
+            <div className='flex justify-between flex-col w-full h-full'>
+              <div className='flex justify-between items-start'>
+                {/* for the ethereum logo */}
+                <div className='w-10 h-10 rounded-full border-2 border-white flex justify-center  items-center'>
+                  <SiEthereum fontSize={21} color='#fff' />
+                </div>
+                <BsInfoCircle fontSize={17} color='#fff' />
+              </div>
+              <div>
+                <p className='text-white font-light text-sm'>Address</p>
+                <p className='text-white font-semibold text-lg mt-1'>
+                  Ethereum
+                </p>
+              </div>
+            </div>
+          </div>
+
+          {/* end of card */}
+          <div className='p-5 sm:w-96 w-full flex-col justify-start items-center blue-glassmorphism'>
+            {/* div for the form */}
+
+            <Input
+              placeholder='Address To'
+              name='addressTo'
+              type='text'
+              handleChange={() => {}}
+            />
+            <Input
+              placeholder='Amount (ETH)'
+              name='amount'
+              type='number'
+              handleChange={() => {}}
+            />
+            <Input
+              placeholder='Keyword (Gif)'
+              name='keyword'
+              type='text'
+              handleChange={() => {}}
+            />
+            <Input
+              placeholder='Enter Message'
+              name='message'
+              type='text'
+              handleChange={() => {}}
+            />
+            <div className='h-[1px] w-full bg-gray-400 my-2' />
+            {false ? (
+              <Loader />
+            ) : (
+              <button
+                type='button'
+                onClick={handleSubmit}
+                className='text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full cursor-pointer'
+              >
+                Send Now
+              </button>
+            )}
           </div>
         </div>
       </div>
